a03f32ef6f995151d54d3853674db9728ad2cd9d
Roland <roland.hof@cubera.ch>
2019-02-09 11:11:09 +0100

    initial checkin

---
+++ src/perc-sharp/perc-sharp.domain/Perceptron.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace perc_sharp.domain
+{
+    public class Perceptron
+    {
+        public decimal Bias;
+        public decimal[] Weight_1;
+
+        public decimal Evaluate(decimal[] input)
+        {
+            return 0;
+        }
+
+        public void AdjustWeights(decimal[] input, int error)
+        {
+            
+        }
+    }
+}
