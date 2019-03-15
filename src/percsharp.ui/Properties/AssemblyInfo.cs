4cacf3bbc8bdab7a30b10cb9015dd965496087bc
Roland Hof <roland.hof@gmail.com>
2019-03-15 16:12:18 +0100

    Configuring Publish

--- src/percsharp.ui/Properties/AssemblyInfo.cs
+++ src/percsharp.ui/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("percsharp.ui")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("percsharp")]
+[assembly: AssemblyDescription("Perceptron Learning Algorithm Viszalizaton")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("percsharp.ui")]
+[assembly: AssemblyProduct("percsharp")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.1")]
+[assembly: AssemblyFileVersion("01.1.0.1")]
+[assembly: NeutralResourcesLanguage("en")]
+
